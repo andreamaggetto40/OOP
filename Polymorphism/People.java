9b17c0b99e72dfb26e98552a5a9828e68526793c
Andrea Maggetto <andreamaggetto40@gmail.com>
2022-08-21 21:48:44 +0200

    Initial commit

---
+++ Polymorphism/People.java
@@ -0,0 +1,41 @@
+package Polymorphism;
+
+public class People {
+    public static class Person{ 
+        @NotNull protected String name;
+        @NotNull protected int age;
+
+        public Person(){};
+        public Person(String name,int age){
+            this.name = name;
+            this.age = age;
+        }
+        @Override
+        public String toString() {
+            return "Name is : " + name + " " + "Age is : " + age;
+        }
+    }
+    public static class Student extends Person{
+        @NotNull protected String college;
+
+        public Student(){super();}
+        public Student(String name, int age, String college){
+            super(name,age);
+            this.college = college;
+        }
+        @Override
+        public String toString() {
+            return super.toString() + "College is : " + college;
+        }
+        /*
+         * A testing main for showcasing the main OOP features
+         */
+        public static void main(String[] args) {
+            Person p1 = new Student("Jon Doe",21,"UCLA"); //Subsumption: First Polymorphism form
+
+            System.out.println(p1); //At run-time, the Student ToString method is invoked; This occurs due to Dynamic Dispatching 
+
+            
+        }
+    }
+}
