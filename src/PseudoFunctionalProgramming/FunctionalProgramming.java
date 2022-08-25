7d6cd55a681649feb25e50213551cce6722dc830
Andrea Maggetto <andreamaggetto40@gmail.com>
2022-08-25 09:50:02 +0200

    Update

--- src/PseudoFunctionalProgramming/FunctionalProgramming.java
+++ src/PseudoFunctionalProgramming/FunctionalProgramming.java
@@ -44,7 +44,7 @@ public class FunctionalProgramming {
 
         items.forEach((n) -> System.out.println(n)); //Lambda function: Consumer
 
-        List<Integer> lengths = map(new Iterable<>(List.of("Hello","World","I'm","John","Doe")),new Function<String,Integer>() {
+        List<Integer> lengths = map(new ArrayList<>(List.of("Hello","World","I'm","John","Doe")),new Function<String,Integer>() {
             @Override
             public Integer apply(String t) {
                 return t.length();
